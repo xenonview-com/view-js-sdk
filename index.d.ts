3a677031f7ade611700d8afab23d7a39dfdfb130
Luke Woydziak <luke.woydziak@gmail.com>
2022-07-27 19:28:19 -0600

    [#182776381] Allow for platform to be specified. -[x] JS SDK - units -fix types

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,15 @@
 declare module 'xenon-view-sdk' {
     interface XenonInterface {
         init: (apiKey: string, apiUrl?: string) => void;
+        platform(softwareVersion: string, deviceModel: string, operatingSystemVersion: string) => void;
         pageView: (page: string) => void;
         funnel: (stage: string, action: string | object) => void;
         outcome: (name:string, action: string | object) => void;
-        event: (event: object) =>void;
+        event: (event: object) => void;
         id: (id?: string) => string;
         commit: () => Promise<object>;
         deanonymize: (person: object) => Promise<object>;
-
+        removePlatform() => void;
     }
     let Xenon: XenonInterface;
     export default Xenon;
